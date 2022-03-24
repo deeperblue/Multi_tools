8fd4ac173172dcd03bb984b31eab1f796f80d460
robin.zhang <robin.zhang@zongmutech.com>
2022-03-24 09:20:37 -0700

    [ADD] Add tools repo

---
+++ tools_repo.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+#gnu tools
+
+
+
+#Frame
+function download_FlameGraph()
+{
+    git clone git@github.com:brendangregg/FlameGraph.git
+}
+
+
+
+
+
+
+#### main ####
+download_FlameGraph
+
+
+
+
+
+
+
